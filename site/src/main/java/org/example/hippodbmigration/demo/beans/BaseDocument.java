6ae03b6680fca4be2ba0892ea854610f2ed6dbe5
Woonsan Ko <woonsan.ko@bloomreach.com>
2018-07-24 11:36:12 -0400

    Initial project creation

---
+++ site/src/main/java/org/example/hippodbmigration/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.example.hippodbmigration.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="hippodbmigrationdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
